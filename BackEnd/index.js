c32be1e6d9fa60a3714deb39fdd2676bbc1027d0
bkm <sniperbika@gmail.com>
2026-03-01 14:15:19 +0200

    server

--- BackEnd/index.js
+++ BackEnd/index.js
@@ -9,7 +9,6 @@ const path = require("path");
 app.use(express.static(path.join(__dirname, "../public")));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-app.use(express.static("../public/"));
 app.use(cors());
 app.use(helmet());
 

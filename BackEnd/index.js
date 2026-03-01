4fb9d80a22fa439b120b4da9891b91ef08476fea
bkm <sniperbika@gmail.com>
2026-03-01 14:14:38 +0200

    server

--- BackEnd/index.js
+++ BackEnd/index.js
@@ -6,8 +6,7 @@ const PORT = process.env.PORT || 5000;
 const app = express();
 const path = require("path");
 
-
-// app.use(express.static(path.join(__dirname, "../")));
+app.use(express.static(path.join(__dirname, "../public")));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static("../public/"));
